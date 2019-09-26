56537f41f80b1d5aab01040be83e7dadee6cf01d
Pedro Camata Andreon <pedrocamata@gmail.com>
2019-09-26 23:40:17 +0100

    Created modal functionalities and demonstration

---
+++ js/bony.js
@@ -0,0 +1,39 @@
+console.log("alegria");
+
+// Get all modals everytime
+document.addEventListener('click', function (event) {
+
+    if (event.target.matches('.modal-btn')
+        || event.target.matches('.modal')) {
+        modalToggle(event.target.target)
+    }
+
+    event.preventDefault();
+}, false);
+
+
+// Get all image everytime
+
+
+// Modal
+function modalToggle(modalId) {
+    var modals = document.getElementsByClassName(modalId);
+    var validId = true;
+
+    if (modals.length === 0) {
+        modals = document.getElementsByClassName('modal');
+        validId = false;
+    }
+
+    for (const i of modals) {
+        if (i.classList.contains('show')) {
+            // hide
+            i.classList.remove('show');
+        } else if (validId) {
+            // show
+            i.classList.add('show');
+        }
+    }
+}
+
+// Image zoom
