0077b24c59b7b4fde63a9494c368c580cb3befce
Pedro Camata Andreon <pedrocamata@gmail.com>
2019-10-09 23:57:36 +0100

    Refactored SCSS to reduce redundancy, created side menu and tabs system

--- js/bony.js
+++ js/bony.js
@@ -2,11 +2,40 @@
 document.addEventListener('click', function (event) {
     if (event.target.matches('.modal-btn')
         || event.target.matches('.modal')) {
-        modalToggle(event.target.target)
+
+        modalToggle(event.target.target);
+
     } else if (event.target.matches('.zoomable')
         && event.target.matches('img')) {
+
         // Zoom img
         document.getElementsByTagName('body')[0].innerHTML += "<div class='modal modal-img-zoom show'><div class= 'modal-body center'><div class= 'modal-close modal-btn'>x</div><img id='img-zoom' class='full' src='" + event.target.currentSrc + "'></div></div>";
+
+    } else if (event.target.matches('.tab-btn')) {
+
+        var btn = event.target;
+        var tabGroup = btn.closest('.tabmenu').getAttribute('tab-group');
+
+        // Hide everything in tabGroup
+        for (const g of document.getElementsByClassName(tabGroup)) {
+            for (const t of g.getElementsByClassName('tab')) {
+                t.classList.remove('show');
+            }
+        }
+
+        // Show active tab in tabGroup
+        for (const i of document.querySelectorAll('.' + tabGroup + ' .' + btn.getAttribute('tab'))) {
+            if (!i.classList.contains('show')) {
+                i.classList.add('show');
+            }
+        }
+
+        for (const b of document.getElementsByClassName("tab-btn")) {
+            b.classList.remove('button-primary');
+            if (b.getAttribute('tab') == btn.getAttribute('tab')) {
+                b.classList.add('button-primary');
+            }
+        }
     }
 
     event.preventDefault();
@@ -35,4 +64,9 @@ function modalToggle(modalId) {
             i.classList.add('show');
         }
     }
+}
+
+// Tab logic
+function tabLogic() {
+
 }
